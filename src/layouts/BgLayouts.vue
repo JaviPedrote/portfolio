822b85beed143fb3e16868204253b32bc440633c
JaviPedrote <pedrote3.1416@gmail.com>
2024-08-27 11:05:14 +0200

    Arreglando BgImage

--- src/layouts/BgLayouts.vue
+++ src/layouts/BgLayouts.vue
@@ -1,18 +1,11 @@
 <template>
   <div
-    className="absolute top-0 bg-black text-white text-2xl w-screen h-screen font-bold underline "
+    className="top-0 bg-black text-white text-2xl font-bold underline w-full"
   >
   <RouterLink to="/">Home</RouterLink>
-  <div className="flex place-content-center pb-0 items-center w-[100%] h-[95%]">
-    <div
-    :style="{ backgroundImage: `url(${backgroundImage})` }"
-    className="absolute bg-cover w-60 h-36 bg-black md:w-68 md:h-36 lg:w-[26rem] lg:h-60"
-    >
-    </div>
-  </div>
 
   </div>
-  <main class="items-center">
+  <main class="flex items-center">
       <RouterView v-slot="{Component}">
         <KeepAlive>
           <component :is="Component"></component>
@@ -23,6 +16,6 @@
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
-import backgroundImage from '../assets/images/Bg.jpg'
+
 
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,86 @@
 <template>
-  <nav className="absolute top-12 flex flex-wrap w-1/3 pl-12 pt-8 ">
-    <div
-      className="flex flex-row flex-wrap bg-black gap-20 text-white text-start"
-    >
-      <RouterLink className=" text-base" to="/Proyecto1">Calculadora de hipotecas<img className="w-48" :src=calcHipoteca alt="Proyecto1"></RouterLink>
-      <RouterLink className=" text-base" to="/Proyecto1">Calculadora de hipotecas<img className="w-48" :src=calcHipoteca alt="Proyecto1"></RouterLink>
+  <div className="m-2 mt-6 flex flex-col text-white gap-10">
 
-      <RouterLink className=" text-base" to="/Proyecto1">Calculadora de hipotecas<img className="w-48" :src=calcHipoteca alt="Proyecto1"></RouterLink>
-      <RouterLink className=" text-base" to="/Proyecto1">Calculadora de hipotecas<img className="w-48" :src=calcHipoteca alt="Proyecto1"></RouterLink>
-      <RouterLink className=" text-base" to="/Proyecto1">Calculadora de hipotecas<img className="w-48" :src=calcHipoteca alt="Proyecto1"></RouterLink>
-      <RouterLink className=" text-base" to="/Proyecto1">Calculadora de hipotecas<img className="w-48" :src=calcHipoteca alt="Proyecto1"></RouterLink>
-
-      
+    <div className="flex flex-wrap w-full h-1/3">
+      <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+        <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+        <img
+          className="w-36 md:w-52 lg:w-[140px]  xl:w-60 h-auto rounded-lg"
+          :src="calcHipoteca"
+          alt="Proyecto1"
+        />
+      </RouterLink>
+      <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+        <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+        <img
+          className="w-36 md:w-52 lg:w-[140px]  xl:w-60 h-auto rounded-lg"
+          :src="calcHipoteca"
+          alt="Proyecto1"
+        />
+      </RouterLink>
+      <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+        <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+        <img
+          className="w-36 md:w-52 lg:w-[140px]  xl:w-60 h-auto rounded-lg"
+          :src="calcHipoteca"
+          alt="Proyecto1"
+        />
+      </RouterLink>
+      <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+        <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+        <img
+          className="w-36 md:w-52 lg:w-[140px] xl:w-60 h-auto rounded-lg"
+          :src="calcHipoteca"
+          alt="Proyecto1"
+        />
+      </RouterLink>
     </div>
-   
-   </nav>
+
+    <div className="w-auto h-1/3">
+      <img :src="backgroundImage" alt="" />
+    </div>
+
+    <div className="flex flex-wrap w-full h-1/3">
+      
+        <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+          <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+          <img
+            className="w-36 md:w-52 lg:w-[140px]  xl:w-60 h-auto rounded-lg"
+            :src="calcHipoteca"
+            alt="Proyecto1"
+          />
+        </RouterLink>
+        <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+          <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+          <img
+            className="w-36 md:w-52 lg:w-[140px]  xl:w-60 h-auto rounded-lg"
+            :src="calcHipoteca"
+            alt="Proyecto1"
+          />
+        </RouterLink>
+        <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+          <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+          <img
+            className="w-36 md:w-52 lg:w-[140px]  xl:w-60 h-auto rounded-lg"
+            :src="calcHipoteca"
+            alt="Proyecto1"
+          />
+        </RouterLink>
+        <RouterLink className="flex flex-col items-center lg:w-auto w-1/2 p-2" to="/Proyecto1">
+          <span className="text-[0.78rem] flex-grow">Calculadora de hipotecas</span>
+          <img
+            className="w-36 md:w-52 lg:w-[140px] xl:w-60 h-auto rounded-lg"
+            :src="calcHipoteca"
+            alt="Proyecto1"
+          />
+        </RouterLink>
+    </div>
+
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import  calcHipoteca from '@/assets/images/Proyecto1.jpg'
-
+import calcHipoteca from '@/assets/images/Proyecto1.jpg'
+import backgroundImage from '../assets/images/Bg.jpg'
 </script>
